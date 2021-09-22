2993e76c3d080efbdda1f3be12c5cfcac38b2205
Julian <julian.bolanos@seratic.com>
2021-09-21 22:46:19 -0500

    se ajustan métodos

--- ejecutarWatershedBackgroundMarkers.m
+++ ejecutarWatershedBackgroundMarkers.m
@@ -1,16 +1,27 @@
 function [labels] = ejecutarWatershedBackgroundMarkers(fgm4,gmag,imageSegmented)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
-bw = imbinarize(imageSegmented(:,:,3));
-imshow(bw)
-title('Background')
-D = bwdist(bw);
-DL = watershed(D);
-bgm = DL == 0;
+%%
+%bw = erodeMask(bw);
+imR=imbinarize(imageSegmented(:,:,3));
+se = strel('disk',6);
+imR2 = imdilate(imR,se);
+BW2 = edge(imR2,'Prewitt');
+BW2 = imdilate(BW2,strel('disk',3));
+imshow(BW2);
+%%
+bgm = BW2;
 imshow(bgm)
 title('Watershed Ridge Lines')
 
-gmag2 = imimposemin(gmag, bgm | fgm4);
+gmag2 = imimposemin(gmag,bgm|fgm4);
+
+%%
 labels = watershed(gmag2);
+%%
+labelsToImage = imdilate(labels==0,ones(3,3)) + 3*fgm4;
+I4 = labeloverlay(imageSegmented(:,:,3),labelsToImage);
+imshow(I4)
+title('Markers and Object Boundaries Superimpo on Original Image')
 end
 
